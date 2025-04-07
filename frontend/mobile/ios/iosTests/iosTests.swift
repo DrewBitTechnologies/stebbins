79ff0ae904fea45741ae84098c6f78c69672ee43
ajhelbig <helbigventures@gmail.com>
2025-04-06 22:05:04 -0700

    readded ios

---
+++ frontend/mobile/ios/iosTests/iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by Drew Helbig on 4/6/25.
+//
+
+import Testing
+@testable import ios
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
