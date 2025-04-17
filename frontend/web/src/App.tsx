1a2b9f8e18e22da27214750f18be343afd94f216
ajhelbig <helbigventures@gmail.com>
2025-04-17 21:00:27 +0000

    need to learn react before getting into auth

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoginButton from './loginButton'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hi Beannn!</h1>
-      <LoginButton />
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
